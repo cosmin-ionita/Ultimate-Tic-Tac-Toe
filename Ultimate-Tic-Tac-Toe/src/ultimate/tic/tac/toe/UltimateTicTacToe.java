a703d4ff0d8992bad1178a77881bc15c92538187
Cristiana <Cristiana@INTEL>
2016-03-12 11:27:23 +0200

    Changed the message

--- Ultimate-Tic-Tac-Toe/src/ultimate/tic/tac/toe/UltimateTicTacToe.java
+++ Ultimate-Tic-Tac-Toe/src/ultimate/tic/tac/toe/UltimateTicTacToe.java
@@ -16,7 +16,7 @@ public class UltimateTicTacToe {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hello, Developpers!");
+        System.out.println("Good morning, Developpers!");
     
     }
     
